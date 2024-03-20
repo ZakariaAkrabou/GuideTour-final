2e8656006a6f27e0ffd588559d86ea956c41cc98
zakariaxw <zakariajr963@gmail.com>
2024-03-20 12:16:19 +0000

    edited file Structure

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,24 @@
+const UserController = {
+    getAllUsers: (req, res) => {
+     
+    },
+  
+    getUserById: (req, res) => {
+      
+    },
+  
+    createUser: (req, res) => {
+    
+    },
+  
+    updateUser: (req, res) => {
+      
+    },
+  
+    deleteUser: (req, res) => {
+     
+    }
+  };
+  
+  module.exports = UserController;
+  
