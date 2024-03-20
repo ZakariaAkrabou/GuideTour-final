e6f254210eaaa060af754d0e5f1c1ddcd183fbdd
zakariaxw <zakariajr963@gmail.com>
2024-03-20 22:21:17 +0000

    file strecture fixed

---
+++ backend/controllers/GuideController.js
@@ -0,0 +1,23 @@
+const GuideController = {
+   
+  
+    showProfile: (req, res) => {
+      
+    },
+  
+    createGuide: async (req, res) => {
+        
+        
+    },
+  
+    updateGuide: (req, res) => {
+      
+    },
+  
+    deleteGuide: (req, res) => {
+     
+    }
+  };
+  
+  module.exports = GuideController;
+  
--- backend/routes/guideRoute.js
+++ backend/routes/guideRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const GuideController = require("../controllers/GuideController");
+const GuideController = require("../controllers/GuideController");
 
-// router.get("/show", GuideController.getAllGuide);
-// router.post("/create", GuideController.createGuide);
-// router.put("/update/:id", GuideController.updateGuide);
-// router.delete("/delete/:id", GuideController.deleteGuide);
+router.get("/profile", GuideController.showProfile);
+router.post("/create", GuideController.createGuide);
+router.put("/update/:id", GuideController.updateGuide);
+router.delete("/delete/:id", GuideController.deleteGuide);
 
 module.exports = router;
