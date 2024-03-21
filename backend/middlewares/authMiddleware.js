b8a08c1f7801460c23b0f0e2a2c89fa0eb113a60
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-03-21 10:46:27 +0000

    add the role of Admin

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,5 @@
-const JWT = require('jsonwebtoken')
-const User = require('../models/User')
-
+const JWT = require('jsonwebtoken');
+const User = require('../models/User');
 
 exports.authenticateUser = async (req, res, next) => {
     const token = req.header("Authorization");
@@ -9,13 +8,16 @@ exports.authenticateUser = async (req, res, next) => {
     }
     try {
         const decoded = JWT.verify(token.replace('Bearer ', ''), 'GAHDYSB');
-        req.userId = decoded.userid;
+        const user = await User.findById(decoded.userid);
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid token: User not found' });
+        }
+        req.user = user;
         next();
     } catch (error) {
         res.status(401).json({ error: 'Invalid token' });
     }
 };
-
 exports.isAdmin = (req, res, next) => {
     if (!req.user || req.user.role !== "admin") {
       return res.status(403).json({ error: "Unauthorized only admin " });
