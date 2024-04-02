50423670e349598443e580fc66dd32391123b283
zakaria akrabou <43080590+ZakariaAkrabou@users.noreply.github.com>
2024-04-02 10:28:27 +0000

    Revert "Socket chat"

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require('path');
 const connectDB = require("./configs/database");
 const userRoutes = require("./routes/userRoute");
 const guideRoutes = require("./routes/guideRoute");
@@ -10,8 +9,7 @@ const bookingRoutes = require("./routes/bookingRoute");
 const reviewRoutes = require("./routes/reviewRoute"); 
 const paymentRoutes = require("./routes/paymentRoute"); 
 const AuthRoute = require('./routes/AuthRoute')
-const initializeSocket = require('./controllers/ChatController')
-const http = require('http');
+const review = require('./routes/reviewRoute')
 
 require("dotenv").config();
 
@@ -22,14 +20,6 @@ app.use(express.json());
 
 connectDB();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/', (req,res)=>{
-//   res.send("Welcome")
-// })
-app.get('/chat/:token', (req, res) => {
-  res.sendFile(path.join(__dirname, '.', 'index.html')); 
-});
 app.use("/api/auth",  AuthRoute);
 app.use("/api/users", userRoutes);
 app.use("/api/guides", guideRoutes);
@@ -40,10 +30,6 @@ app.use("/api/bookings", bookingRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-//Socket Server
-const server = http.createServer(app);
-initializeSocket(server);
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
