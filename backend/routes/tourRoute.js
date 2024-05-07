21bc34f727f83877b242aa605144f3a7daec428f
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-05-07 11:34:20 +0100

    add all tours crud

--- backend/routes/tourRoute.js
+++ backend/routes/tourRoute.js
@@ -7,7 +7,8 @@ const tourSwagger = require('../swagger/tourSwagger');
 router.swagger = tourSwagger;
 
 router.post('/create',authenticateUser,isGuide,uploadTour.single("image"), tourController.createTour);
-router.get('/allTours', /*authenticateUser,*/tourController.getAllTours);
+router.get('/allGuideTours', /*authenticateUser,*/tourController.getGuideTours);
+router.get('/allTours',tourController.getAllTours )
 router.get('/getTour/:id', authenticateUser,tourController.getTourById);
 router.put('/updateTour/:id', authenticateUser, isGuide, uploadTour.single('image'), tourController.updateTour);
 router.delete('/deleteTour/:id', authenticateUser,isGuide,tourController.deleteTour);
