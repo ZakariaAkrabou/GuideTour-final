8fcacb150e554b9e99776d3fdf35215b7d7f703e
Rajae-Laalalma <rajaerajaela@gmail.com>
2024-05-13 16:51:21 +0100

    update

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Sidebar from '../components/dashboardGuides/sideBar'
+import TopBar from '../components/dashboardGuides/topBar'
+import {Outlet} from 'react-router-dom'
 
 function dashboard() {
   return (
-    <div>dashboard</div>
+    <>
+<div> <Sidebar/></div>
+    
+    <div><TopBar/></div> 
+    <div>{Outlet}</div>
+    
+    </>
+   
   )
 }
 
