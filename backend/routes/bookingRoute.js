526b52aa733961483335c38add07df122da81bb8
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-05-28 11:43:54 +0100

    updates

--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -2,13 +2,13 @@ const express = require("express");
 const BookingController = require("../controllers/BookingController");
 const { authenticateUser } = require("../middlewares/authMiddleware");
 const router = express.Router();
+
 const bookingSwagger = require('../swagger/bookingSwagger');
 router.swagger = bookingSwagger;
 
 
-router.post("/book", authenticateUser, BookingController.bookService);
-router.get("/checkout/:id", authenticateUser,BookingController.CheckoutSession);
-router.get("/pay", authenticateUser,BookingController.createBooking);
 
-
+router.post("/book", BookingController.getCheckoutSession);
+router.post("/payment-success",authenticateUser, BookingController.paymentSuccess);
+router.post("/payment-failed",authenticateUser, BookingController.paymentFailed);
 module.exports = router;
